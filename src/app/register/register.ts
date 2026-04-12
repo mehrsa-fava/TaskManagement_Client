9e21f2c2e3dd39f66bef8fac95305cdb7abb8fbb
mehrsa amini <1273130367@C1813735>
2026-04-12 10:42:02 +0330

    UseReactiveFormForRegister

--- src/app/register/register.ts
+++ src/app/register/register.ts
@@ -1,23 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
 import { UserService } from '../services/user-service';
 import { AuthService } from '../services/auth-service';
 import { getApiErrorMessage } from '../utils/api-error.util';
 
 @Component({
   selector: 'app-register',
-  imports: [FormsModule, RouterLink],
+  imports: [ReactiveFormsModule, RouterLink],
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
 export class Register implements OnInit {
-  firstName = '';
-  lastName = '';
-  email = '';
-  username = '';
-  password = '';
-  confirmPassword = '';
+  registerForm!: FormGroup;
+  //firstName = '';
+  //lastName = '';
+  //email = '';
+  //username = '';
+  //password = '';
+  //confirmPassword = '';
   errorMessage = '';
 
   private readonly minPasswordLength = 8;
@@ -25,46 +26,50 @@ export class Register implements OnInit {
   constructor(
     private userService: UserService,
     private authService: AuthService,
-    private router: Router
+    private router: Router,
   ) {}
 
   ngOnInit(): void {
     if (this.authService.isLoggedIn()) {
       this.router.navigate(['/task/list']);
     }
+
+    this.registerForm = new FormGroup({
+      firstName: new FormControl('', [Validators.required]),
+      lastName: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      username: new FormControl('', [Validators.required]),
+      password: new FormControl('', [Validators.required]),
+      confirmPassword: new FormControl('', [Validators.required]),
+    });
   }
 
   submit(): void {
     this.errorMessage = '';
 
-    if (this.password.length < this.minPasswordLength) {
+    const { firstName, lastName, email, username, password, confirmPassword } =
+      this.registerForm.value;
+
+    if (password.length < this.minPasswordLength) {
       this.errorMessage = `Password must be at least ${this.minPasswordLength} characters.`;
       return;
     }
-    if (this.password !== this.confirmPassword) {
+    if (password !== confirmPassword) {
       this.errorMessage = 'Password and Confirm password do not match.';
       return;
     }
 
-    this.userService
-      .register(
-        this.firstName,
-        this.lastName,
-        this.email,
-        this.username,
-        this.password
-      )
-      .subscribe({
-        next: (response) => {
-          if (response.statusCode === 200) {
-            this.router.navigate(['/login']);
-          } else {
-            this.errorMessage = response.message;
-          }
-        },
-        error: (err) => {
-          this.errorMessage = getApiErrorMessage(err);
-        },
-      });
+    this.userService.register(firstName, lastName, email, username, password).subscribe({
+      next: (response) => {
+        if (response.statusCode === 200) {
+          this.router.navigate(['/login']);
+        } else {
+          this.errorMessage = response.message;
+        }
+      },
+      error: (err) => {
+        this.errorMessage = getApiErrorMessage(err);
+      },
+    });
   }
 }
